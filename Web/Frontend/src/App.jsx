246fe1a73aef36e0f584864e9fd9540cbeeb12ef
Nagraj-13 <nagrajy131200350@gmail.com>
2024-10-09 20:35:56 +0530

    initial commit

---
+++ Web/Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h2 className='text-xl font-bold w-[100%] bg-slate-400 flex justify-center'>Hey this is Khushi</h2>
+    </>
+  )
+}
+
+export default App
